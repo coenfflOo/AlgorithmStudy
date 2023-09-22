990b69aea8904ee9a8c9100fe1fda60b1615cff2
coenffl <>
2023-09-22 21:04:57 +0900

    Sep_22nd

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import java.util.*;
 import java.io.*;
-public class bj1018 {
+
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf((A+B)*(A-B)));
+        bw.flush();
     }
 }

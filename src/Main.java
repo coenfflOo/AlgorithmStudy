ffa0eafa8c6050d67dbdf6d53ba3ccd6e9c2ceba
coenffl <>
2023-09-29 08:23:53 +0900

    Sep_29th

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(lcm(A,B)+"\n");
         }
-
-        int[] result = arr.clone();
-        arr = Arrays.stream(arr).distinct().toArray();
-        Arrays.sort(arr);
-        for (int number :
-                result) {
-            bw.write(findIndex(number, arr) + " ");
-        }
-        bw.close();
+        System.out.print(sb);
     }
 
-    private static int findIndex(int n, int[] arr) {
-        int left = 0;
-        int right = arr.length - 1;
+    private static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == n) {
-                return mid; // 원하는 요소를 찾은 경우 해당 인덱스를 반환
-            } else if (arr[mid] < n) {
-                left = mid + 1; // 중간 값보다 큰 경우 오른쪽 반을 탐색
-            } else {
-                right = mid - 1; // 중간 값보다 작은 경우 왼쪽 반을 탐색
-            }
-        }
-        return -1;
+    private static int lcm(int x, int y) {
+        int a = Math.min(x,y);
+        int b = Math.max(x,y);
+        return a * b / gcd(a, b);
     }
 }
---
+++ src/September/bj1735.java
@@ -0,0 +1,33 @@
+package September;
+import java.util.*;
+import java.io.*;
+public class bj1735 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long sonA = Integer.parseInt(st.nextToken());
+        long momA = Integer.parseInt(st.nextToken());
+
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        long sonB = Integer.parseInt(st2.nextToken());
+        long momB = Integer.parseInt(st2.nextToken());
+
+        long lcmNum = lcm(momA,momB);
+        long sonR = sonA*(lcmNum/momA) + sonB*(lcmNum/momB);
+
+        long gcdNum = gcd(sonR, lcmNum);
+        System.out.println((sonR/gcdNum)+" "+(lcmNum/gcdNum));
+
+    }
+    private static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    private static long lcm(long x, long y) {
+        long a = Math.min(x,y);
+        long b = Math.max(x,y);
+        return a * b / gcd(a, b);
+    }
+}

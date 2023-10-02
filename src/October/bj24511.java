682a711f72c1650a84b841295de5dce7f357c55a
coenffl <>
2023-10-03 00:02:39 +0900

    Oct_2nd

--- src/October/bj24511.java
+++ src/October/bj24511.java
@@ -51,36 +51,42 @@ public class bj24511 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // N: 자료구조의 개수
 
-        int[] typeArr = new int[N];
+        int[] typeArr = new int[N]; // 각 자료구조의 타입(0: 큐, 1: 스택)을 저장하는 배열
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i < N; i++){
+        // 각 자료구조의 타입 정보를 입력받아 typeArr에 저장
+        for (int i = 0; i < N; i++) {
             typeArr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Deque<Integer> deque = new ArrayDeque<>();
+        Deque<Integer> deque = new ArrayDeque<>(); // 원소를 저장하기 위한 Deque 자료구조
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if(typeArr[i] == 0){
+
+            // 현재 자료구조가 큐인 경우 뒤에 원소를 삽입
+            if (typeArr[i] == 0) {
                 deque.addLast(num);
             }
         }
 
-        int M = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine()); // M: 삽입할 원소의 개수
 
         st = new StringTokenizer(br.readLine());
         br.close();
-        while(M --> 0){
-            int moveValue = Integer.parseInt(st.nextToken());
-            deque.addFirst(moveValue);
-            sb.append(deque.pollLast()).append(" ");
+
+        // 원소를 삽입하고 리턴값을 구하는 과정을 반복
+        while (M --> 0) { // M을 하나씩 감소하며 반복
+            int moveValue = Integer.parseInt(st.nextToken()); // 삽입할 원소
+
+            deque.addFirst(moveValue); // 현재 자료구조에 원소를 삽입
+            sb.append(deque.pollLast()).append(" "); // 마지막 자료구조에서 원소를 빼고 결과 문자열에 추가
         }
 
-        System.out.println(sb);
+        System.out.println(sb); // 결과 문자열 출력
     }
 }

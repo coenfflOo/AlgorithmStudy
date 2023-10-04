13d89341191a7c08fd6582b99a1b81c793ceda24
coenffl <>
2023-10-04 10:25:55 +0900

    Oct_4th

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,31 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        System.out.println((int)Math.pow(2,N));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+        }
+        Arrays.sort(arr);
+
+        if (N == 1){
+            System.out.println((int)Math.pow(arr[0],2));
+        } else {
+            System.out.println(findN(arr));
+        }
+    }
+
+    private static int findN(int[] arr){
+        int index = arr.length/2;
+        int n;
+        if (arr.length%2==0)
+            n = arr[index - 1] * arr[index];
+        else
+            n=arr[index - 1] * arr[index+1];
+        return n;
     }
 }
---
+++ src/October/bj20920.java
@@ -0,0 +1,44 @@
+package October;
+import java.util.*;
+import java.io.*;
+public class bj20920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Map<String, Integer> wordPaper = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            if (word.length()>=M){
+                wordPaper.put(word,wordPaper.getOrDefault(word,0)+1);
+            }
+        }
+
+
+        List<String> keySet = new ArrayList<>(wordPaper.keySet());
+        // 알파벳 사전 순
+        Collections.sort(keySet);
+
+        keySet.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int freqCompare = wordPaper.get(o2).compareTo(wordPaper.get(o1));
+                if (freqCompare != 0) {
+                    return freqCompare; // 자주 나오는 단어 비교
+                }
+                int lengthCompare = Integer.compare(o2.length(), o1.length());
+                if (lengthCompare != 0) {
+                    return lengthCompare; // 단어 길이 비교
+                }
+                return o1.compareTo(o2); // 알파벳 순 비교
+            }
+        });
+
+        for (String key : keySet) {
+            bw.write(key+"\n");
+        }
+        bw.flush();
+    }
+}

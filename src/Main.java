0283ea1451ab734a53c58ae2beecd377702f6eb1
coenffl <>
2023-10-11 15:27:10 +0900

    Oct_11th

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int R;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
+            String str = br.readLine();
+            R = 1;
+            sb.append(isPalindrome(str)).append(" ").append(R).append("\n");
         }
-        Arrays.sort(arr);
-
-        if (N == 1){
-            System.out.println((int)Math.pow(arr[0],2));
-        } else {
-            System.out.println(findN(arr));
-        }
+        bw.write(sb.toString());
+        bw.flush();
     }
 
-    private static int findN(int[] arr){
-        int index = arr.length/2;
-        int n;
-        if (arr.length%2==0)
-            n = arr[index - 1] * arr[index];
-        else
-            n=arr[index - 1] * arr[index+1];
-        return n;
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+
+    public static int recursion(String s, int l, int r){
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else {
+            R++;
+            return recursion(s, l + 1, r - 1);
+        }
     }
 }

00614838e09593034cdbbf4489dac9a654574abd
coenffl <>
2023-10-18 23:30:29 +0900

    Oct_18th

---
+++ src/October/bj1697.java
@@ -0,0 +1,10 @@
+package October;
+import java.util.*;
+import java.io.*;
+public class bj1697 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}

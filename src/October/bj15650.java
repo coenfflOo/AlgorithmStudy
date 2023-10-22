518b1809a163d75aab40332e97970394b9804400
coenffl <>
2023-10-22 21:06:59 +0900

    Oct_22th

---
+++ src/October/bj15650.java
@@ -0,0 +1,36 @@
+package October;
+
+import java.util.*;
+import java.io.*;
+
+public class bj15650 {
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        dfs(1,0);
+    }
+
+    public static void dfs(int at, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+
+        for (int i = at; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+}

fbcd76175e331f39d836e4522d8f47f73392f8f5
coenfflOo <minipeach0923@gmail.com>
2025-01-22 18:44:34 +0900

    20250122

---
+++ src/java_2025/Jan/BOJ11052.java
@@ -0,0 +1,24 @@
+package java_2025.Jan;
+import java.util.*;
+import java.io.*;
+public class BOJ11052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] p = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = Math.max(dp[j]+p[i-j], dp[i]);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}

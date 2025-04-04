3895c4a1370cc13db0390f3e39160466ea7074ce
coenfflOo <minipeach0923@gmail.com>
2025-04-04 16:05:38 +0900

    20250404

--- src/java_2025/Mar/BOJ2805.java
+++ src/java_2025/Mar/BOJ2805.java
@@ -10,21 +10,20 @@ public class BOJ2805 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         long[] tree = new long[n];
-        long max = 0;
+        long low = 0;
+        long high = -1;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             tree[i] = Long.parseLong(st.nextToken());
-            max = Math.max(max, tree[i]);
+            high = Math.max(high, tree[i]);
         }
 
-        long low = 0;
-        long high = max;
         while (low < high) {
             long mid = (low+high)/2;
             long length = 0;
 
             for(long t: tree) {
-                if(t-mid>0) length += t-mid;
+               length += Math.max(0, t-mid);
             }
 
             if (length >= m) low = mid + 1;

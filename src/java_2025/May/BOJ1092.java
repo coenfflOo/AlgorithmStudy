7a35b1b64e7bbaae1675a4cb17568cbd23d92aa5
coenfflOo <minipeach0923@gmail.com>
2025-05-02 19:33:30 +0900

    20250502

---
+++ src/java_2025/May/BOJ1092.java
@@ -0,0 +1,44 @@
+package java_2025.May;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class BOJ1092 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Integer[] crains = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            crains[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        List<Integer> boxes = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            boxes.add(Integer.parseInt(st.nextToken()));
+        }
+        Arrays.sort(crains,Comparator.reverseOrder());
+        boxes.sort(Collections.reverseOrder());
+
+        if (boxes.get(0)>crains[0]) {
+            System.out.println("-1");
+            return;
+        }
+        int minutes = 0;
+        while (!boxes.isEmpty()) {
+            int b = 0;
+            int c = 0;
+
+            while (c<n){
+                if (b==boxes.size()) break;
+                else if (crains[c]>=boxes.get(b)) {
+                    boxes.remove(b);
+                    c++;
+                } else b++;
+            }
+            minutes++;
+        }
+        System.out.println(minutes);
+    }
+}

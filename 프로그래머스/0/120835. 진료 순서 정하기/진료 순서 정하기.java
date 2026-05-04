11a742755b3c8126b3106658b359143fed57c60c
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-05-04 13:23:04 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        for(int i=0; i<emergency.length;i++){
+            int cur = 0;
+            for(int j=0; j<emergency.length;j++){
+                if(emergency[i]<=emergency[j])cur++;
+            }
+            answer[i]=cur;
+        }
+        return answer;
+    }
+}

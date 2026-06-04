7ed8114dfb90b781e215bac3477e2a3fc90ed176
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-06-05 01:53:52 +0900

    [level 0] Title: 2의 영역, Time: 1.26 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int start = -1;
+        int end = -1;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==2){
+                start = i;
+                break;
+            }
+        }
+        if(start == -1){
+            return new int[]{-1};
+        }
+        for(int i=arr.length-1; i>=start; i--){
+            if(arr[i]==2){
+                end = i;
+                break;
+            }
+        }
+        if(end == start){
+            return new int[]{arr[start]};
+        }
+        int[] answer = new int[end-start+1];
+        for(int i=start; i<=end; i++){
+            answer[i-start] = arr[i];
+        }
+        return answer;
+    }
+}

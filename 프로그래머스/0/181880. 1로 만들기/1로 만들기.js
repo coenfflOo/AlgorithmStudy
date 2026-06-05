d1af34a53787c3cf23fe5a298022cfbc4b216859
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-06-06 02:12:41 +0900

    [level 0] Title: 1로 만들기, Time: 0.11 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    var answer = 0;
+    for(let i=0; i<num_list.length; i++){
+        var n = num_list[i];
+        while(n!=1){
+            if(n%2==0){
+                n/=2;
+            } else {
+                n = (n-1)/2;
+            }
+            answer++;
+        }
+    }
+    return answer;
+}

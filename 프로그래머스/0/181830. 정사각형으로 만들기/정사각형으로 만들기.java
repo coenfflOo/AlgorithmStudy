2cb30419e24e30c976a0a859d2278bbce82991cf
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-06-14 00:14:58 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.29 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] solution(int[][] arr) {
+        if(arr.length<arr[0].length){
+            int[][] answer = new int[arr[0].length][arr[0].length];
+            for(int i=0;i<arr.length; i++){
+                for(int j=0;j<arr[i].length;j++){
+                    answer[i][j] = arr[i][j];
+                }
+            }
+            return answer;
+        } else if(arr.length>arr[0].length){
+            int[][] answer = new int[arr.length][arr.length];
+            for(int i=0;i<arr.length; i++){
+                for(int j=0;j<arr[i].length;j++){
+                    answer[i][j] = arr[i][j];
+                }
+            }
+            return answer;
+        }else return arr;
+    }
+}

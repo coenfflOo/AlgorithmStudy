17db82076b04ae83327cf28376af4d6c730c408d
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-06-14 19:49:19 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        while(order>0){
+            if((order%10)!=0&&(order%10)%3==0) answer++;
+            order/=10;
+        }
+        return answer;
+    }
+}

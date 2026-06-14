999413efe387fa451649b3c721b15d8a01d31236
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-06-14 18:22:09 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int t) {
+        int answer = n;
+        while (t-->0){
+            answer *=2;
+        }
+        return answer;
+    }
+}

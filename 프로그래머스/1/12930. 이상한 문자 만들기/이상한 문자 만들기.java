d91cc4a527673f4719a690071b12614c4da24866
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-06-25 11:19:16 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 4.84 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int index = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==' ') {
+                answer+=" ";
+                index = 0;
+            }else {
+                if(index%2==0) {
+                    answer+=String.valueOf(s.charAt(i)).toUpperCase();
+                } else answer+=String.valueOf(s.charAt(i)).toLowerCase();
+                index++;
+            }
+        }
+        return answer;
+    }
+}

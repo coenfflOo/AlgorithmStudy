9e17ad9b1e2f409478e04d4755f174cbd30a8051
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-06-26 19:04:22 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        String s =  Integer.toString(n, 3);
+        StringBuilder sb = new StringBuilder(s);
+        s = sb.reverse().toString();
+        int answer = Integer.parseInt(s,3);
+        return answer;
+    }
+}

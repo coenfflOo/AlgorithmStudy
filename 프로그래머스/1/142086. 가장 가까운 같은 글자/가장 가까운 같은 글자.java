78dce51818117f6ac4fbd2e7e21f75fc7762c33f
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-06-27 00:16:47 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 0.37 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        int[] alpha = new int[26];
+        int[] answer = new int[s.length()];
+        Arrays.fill(alpha, -1);
+        for(int i=0; i<s.length();i++){
+            int index = s.charAt(i)-'a';
+            if(alpha[index] == -1) answer[i] = -1;
+            else answer[i] = i - alpha[index];
+            alpha[index] = i;
+        }
+        return answer;
+    }
+}

b808069701f1416266e29eca667f98a13faa6200
하이서 <103016453+say-hiseo@users.noreply.github.com>
2026-07-02 12:28:26 +0900

    [level 1] Title: 소수 찾기, Time: 114.93 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -4,13 +4,13 @@ class Solution {
         number[2] = 1;
         for(int i=3;i<=n;i++){
             int cnt = 0;
-            if(i%2==0){
+            if(i%2==0){ // 짝수 컷
                 number[i] = number[i-1];
                 continue;
             }
             boolean flag = true;
-            for(int j=3; j<=Math.sqrt(i); j++){
-                if(i%j==0) {
+            for(int j=3; j<=Math.sqrt(i); j++){ // 3부터 제곱근까지 확인
+                if(i%j==0) { // 하나라도 걸리면 컷
                     number[i] = number[i-1];
                     flag = false;
                     break;
